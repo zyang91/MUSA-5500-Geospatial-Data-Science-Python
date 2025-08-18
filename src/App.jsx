d86fb7bda973f9adf5c20c48367cd6b762ffcc06
Xiaojiang Li <lixiaojiang.gis@gmail.com>
2025-08-18 08:02:10 -0400

    Fix case-sensitive import for Syllabus

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { HashRouter as Router, Route, Routes, Link } from "react-router-dom";
 // import { HashRouter as Router } from 'react-router-dom';
 
-import Schedule from "./pages/Schedule";
+import Syllabus from "./pages/Syllabus";
 import Home from "./pages/Home";
 import LecturePage from "./pages/LecturePage";
 import Syllabus from "./pages/syllabus";
